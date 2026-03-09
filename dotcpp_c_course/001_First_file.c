3fd9c90efa92b273d13d9ec75745cf097e3c27fd
WilliamZhou <zhouhb2615@outlook.com>
2026-03-09 16:56:37 +0800

    learn sth new

---
+++ dotcpp_c_course/001_First_file.c
@@ -0,0 +1,13 @@
+#include<stdio.h>/*头文件*/ 
+int main()//You can type like this maybe 主函数 
+{
+	
+	int number=2006;//声明变量 并赋值 
+	printf("hello world\n");//printf()输出，\n换行 
+	printf("I was born in %d",number);
+	return 0;// 主函数为int时需写 
+	
+ } 
+ /*注释也可以
+                分成多行打
+					Just like this*/
---
+++ dotcpp_c_course/003_basic_data_type_and_key_words.c
@@ -0,0 +1,28 @@
+/*
+类型关键字       类型名称       占字节数
+char             字符型         1
+unsigned char    无符号字符型   1
+int              整型           4
+unsigned int     无符号整型     4
+short            短整型         2
+unsigned short   无符号短整型   2
+long             长整型         4
+unsigned long    无符号长整型   4
+float            单精度浮点数   4
+double           双精度浮点数   8 
+long double      长双精度浮点数 10
+long long        长整型         8
+
+*/
+
+/*C语言中的32个关键字
+auto	double	int	struct
+break	else	long	switch
+case	enum	register	typedef
+char	extern	return	union
+const	float	short	unsigned
+continue	for	signed	void
+default	goto	sizeof	volatile
+do	if	static	while
+
+*/

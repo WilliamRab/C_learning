ce14fb63331c5b36573080e132db7bce50aaeb92
WilliamZhou <zhouhb2615@outlook.com>
2026-03-30 17:12:14 +0800

    26.3.30

---
+++ from_NBUT/作业/第一次试验/3-3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("请输入一个两位数：");
+	scanf("%d",&a);
+	printf("%d",a/10+a%10*10);
+	return 0;
+	
+	
+} 
---
+++ from_NBUT/大写转小写.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	char x= getchar();
+	char y=x+32;
+	putchar(y);
+	return 0 ;
+	
+	
+}
